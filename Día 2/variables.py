813e0f4893b76f4bdd6b6fbea77472e79fed6058
Diana <diana1310fg@gmail.com>
2022-07-25 13:47:45 -0500

    Variables

---
+++ Día 2/variables.py
@@ -0,0 +1,7 @@
+nombre = input("Dime tu nombre: ")
+edad = input("Dime tu edad: ")
+print("Tu nombre es " + nombre + " y tienes " + edad + " años de edad.")
+
+edad1 = 10
+edad2 = 5
+print(edad1 + edad2)
