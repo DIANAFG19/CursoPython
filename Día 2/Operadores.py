a43aaeacf6984b5ec917b0e47cd0e64600dd5d8b
Diana <diana1310fg@gmail.com>
2022-07-26 23:51:43 -0500

    Operadores Matemáticos.

---
+++ Día 2/Operadores.py
@@ -0,0 +1,18 @@
+x = 6
+y = 2
+print(f"{x} + {y} = {x+y}")
+print(f"{x} - {y} = {x-y}")
+print(f"{x} * {y} = {x*y}")
+print(f"{x} / {y} = {x/y}")
+
+z = 7
+print(f"{z} / {y} = {z/y}")
+print(f"{z} división al piso {y} = {z//y}")
+
+print(f"{z} módulo (%) {y} = {z%y}")
+
+print(f"{z} elevado a la {y} = {z**y}")
+
+print(f"{z} elevado a la {3} = {z**3}")
+
+print(f"La raíz cuadrada de {x} = {x**0.5}")
