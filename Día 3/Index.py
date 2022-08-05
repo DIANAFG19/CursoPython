49d0ee3346f7bb96d1748ba17ddb5a04606b78cb
Diana <diana1310fg@gmail.com>
2022-08-04 21:42:59 -0500

    Método Index().

---
+++ Día 3/Index.py
@@ -0,0 +1,15 @@
+mi_texto = "Esta es una prueba"
+resultado = mi_texto[0]
+print(resultado)
+
+resultado3 = mi_texto[-4]
+print(resultado3)
+
+resultado4 = mi_texto.index("n")
+print(resultado4)
+
+resultado5 = mi_texto.index("a", 5, 15)
+print(resultado5)
+
+resultado6 = mi_texto.rindex("a")
+print(resultado6)
