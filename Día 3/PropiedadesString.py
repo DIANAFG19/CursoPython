b2fdac28a8d7d0b041236fb016d56f1e05501c4c
Diana <diana1310fg@gmail.com>
2022-08-10 11:04:59 -0500

    Propiedades de String.

---
+++ Día 3/PropiedadesString.py
@@ -0,0 +1,26 @@
+# INMUTABLE
+nombre = "Carina"
+# nombre[0] = "K" # TypeError: 'str' object does not support item assignment
+nombre = "Karina"
+print(nombre)
+
+# CONCATENABLE
+n1 = "Kari"
+n2 = "na"
+print(n1 + n2)
+
+# MULTIPLICABLE
+print(n1 * 10)
+
+# MULTILÍNEA
+poema = """Mil pequeños peces blancos
+como si hirviera
+el color del agua"""
+print(poema)
+
+# VERIFICAR SI CONTIENE
+print("agua" in poema)
+print("agua" not in poema)
+print("sol" not in poema)
+print("sol" in poema)
+
