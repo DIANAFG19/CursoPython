516d2ce1fb3d4cc8de06ca298c0a97760ded092e
Diana <diana1310fg@gmail.com>
2022-09-26 22:37:02 -0500

    Operadores lógicos.

---
+++ Día 4/OperadoresLogicos.py
@@ -0,0 +1,26 @@
+print("************** AND **************")
+mi_bool1 = 4 < 5 and 4 < 6
+print(mi_bool1)
+
+mi_bool2 = 4 > 5 and 5 < 6
+print(mi_bool2)
+
+mi_bool3 = 4 > 5 and 4 < 6
+print(mi_bool3)
+
+print("************** OR **************")
+mi_bool1 = 4 < 5 or 4 < 6
+print(mi_bool1)
+
+mi_bool2 = 4 > 5 or 5 > 6
+print(mi_bool2)
+
+mi_bool3 = 4 > 5 or 4 < 6
+print(mi_bool3)
+
+print("************** NOT **************")
+mi_bool1 = not(4 < 5)
+print(mi_bool1)
+
+mi_bool2 = not(4 > 5)
+print(mi_bool2)
