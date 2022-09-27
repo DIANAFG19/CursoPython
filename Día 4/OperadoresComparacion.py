297b1b9b9b47453548eba2445bce8b7fb0620ee2
Diana <diana1310fg@gmail.com>
2022-09-26 22:10:45 -0500

    Operadores de comparación.

--- Día 4/OperadoresComparacion.py
+++ Día 4/OperadoresComparacion.py
@@ -1,1 +1,23 @@
 booleano1 = 10 == 25
+print(booleano1)
+
+booleano2 = 5 + 5 == 10 - 0
+print(booleano2)
+
+booleano3 = 'blanco' == 'Blanco'.lower()
+print(booleano3)
+
+booleano4 = 'blanco' != 'Blanco'.lower()
+print(booleano4)
+
+booleano5 = 100 > 25
+print(booleano5)
+
+booleano6 = 100 < 25
+print(booleano6)
+
+booleano7 = 100 >= 100
+print(booleano7)
+
+booleano8 = 100 <= 101
+print(booleano8)
