615ee47756ae8f3ff74ff06ae6dde49faccba6a2
Diana <diana1310fg@gmail.com>
2023-01-12 23:58:07 -0600

    Loop while y palabras reservadas pass, break y continue en los loops.

---
+++ Día 4/LoopWhile.py
@@ -0,0 +1,31 @@
+monedas = 5
+while monedas > 0:
+    print(f"Tengo {monedas} monedas.")
+    monedas -= 1
+else:
+    print("No tengo más monedas")
+
+####################################################################################
+respuesta = 's'
+while respuesta == 's':
+    respuesta = input("¿Quieres continuar? (s/n): ")
+else:
+    print("Gracias por visitarnos.")
+
+####################################################################################
+while respuesta == 's':
+    pass
+print("Hola.")
+
+####################################################################################
+nombre =input("Ingresa tu nombre: ")
+for letra in nombre:
+    if letra == 'a':
+        break
+    print(letra)
+
+####################################################################################
+for letra in nombre:
+    if letra == 'a':
+        continue
+    print(letra)
